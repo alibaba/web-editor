61757d5c3d5a54d96516f01e07848c2ecbf7977b
codeskyblue <codeskyblue@gmail.com>
2017-03-31 14:41:40 +0800

    allow cookie cors

--- weditor/__main__.py
+++ weditor/__main__.py
@@ -89,6 +89,7 @@ class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Credentials", "true") # allow cookie
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
 
     def options(self, *args):
